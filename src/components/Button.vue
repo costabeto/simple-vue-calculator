ec525a4eceea2d068df9496c73a5c3760f8ba5fe
doravanteBeto <roberto.araujo339@gmail.com>
2020-06-18 01:53:26 -0300

    Finished

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,10 +1,70 @@
 <template>
-  <button class="button">0</button>
+  <button class="button"
+    @click="$emit('onClick', label)"
+    :class="{ operation, double, triple}">
+    {{ label }}
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+      label: {},
+      operation: { type: Boolean },
+      double: { type: Boolean },
+      triple: { type: Boolean },
+    }
+};
 </script>
 
 <style>
+
+:root {
+
+    --bg-button: #f0f0f0;
+    --border-button: solid 1px #888;
+
+}
+
+.button {
+
+    font-size: 1.4em;
+    background-color: var(--bg-button);
+    border: none;
+    border-right: var(--border-button);
+    border-bottom: var(--border-button);
+    outline: none;
+
+}
+
+.button:active {
+
+    background-color: #ccc;
+
+}
+
+.button.double {
+
+    grid-column: span 2;
+
+}
+
+.button.triple {
+
+    grid-column: span 3;
+
+}
+
+.button.operation {
+
+    background-color: #e8f044;
+
+}
+
+.button.operation:active {
+
+    background-color: #e8f044cc;
+
+}
+
 </style>
